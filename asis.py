65d6e84b70a83dcfc7db9d101546d22f33aa869f
William <s3864077@rmit.edu.vn>
2021-06-23 20:06:43 +0700

    update author
    update author

--- asis.py
+++ asis.py
@@ -4,13 +4,14 @@ from pathlib import Path
 
 session = HTMLSession()
 
+
 def get_album(url):
     # url = input("Enter asiansister.com link here: ")
     folder_name = get_album_title(url)
     Path(folder_name).mkdir(parents=True, exist_ok=True)
     r = session.get(url)
-    rs = r.html.find(".lazyload", first = False)
-    
+    rs = r.html.find(".lazyload", first=False)
+
     for x in rs:
         img_url = "https://asiansister.com/" + x.attrs['dataurl'][5:]
         filename = img_url.split('/')[-1]
@@ -19,9 +20,10 @@ def get_album(url):
         file.write(response.content)
         file.close()
 
+
 def get_album_title(url):
     r = session.get(url)
-    rs = r.html.find("h1", first = True)
+    rs = r.html.find("h1", first=True)
     return rs.text
 
 
